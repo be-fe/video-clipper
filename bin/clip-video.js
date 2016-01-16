c444e42c5cb754f8981ed9344c3144199a470149
zhengliangliang <zhengliangliang@baidu.com>
2016-01-16 20:47:04 +0800

    rename dir to common/

--- bin/clip-video.js
+++ bin/clip-video.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var clipperFactory = require(__dirname + '/../server/clipper');
+var clipperFactory = require(__dirname + '/../main/clipper');
 
 var clipper = clipperFactory.getClipper();
 clipper.clipAllVideosUnderVideoLocalFolder();
